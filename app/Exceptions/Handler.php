ada076382c03c592580559e3ecf3ff6a1ed0ae7e
Tippin <richard.tippin@gmail.com>
2020-12-09 22:16:04 -0500

    updated handler to better manage model not found errors.

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,7 +2,14 @@
 
 namespace App\Exceptions;
 
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Str;
+use ReflectionClass;
+use ReflectionException;
+use RTippin\Messenger\Exceptions\InvalidMessengerProvider;
 use Throwable;
 
 class Handler extends ExceptionHandler
@@ -13,7 +20,7 @@ class Handler extends ExceptionHandler
      * @var array
      */
     protected $dontReport = [
-        //
+        InvalidMessengerProvider::class,
     ];
 
     /**
@@ -36,5 +43,51 @@ class Handler extends ExceptionHandler
         $this->reportable(function (Throwable $e) {
             //
         });
+    }
+
+    /**
+     * Render an exception into an HTTP response.
+     *
+     * @param Request $request
+     * @param Throwable $exception
+     * @return JsonResponse
+     * @throws Throwable
+     * @noinspection PhpMissingParamTypeInspection
+     */
+    public function render($request, Throwable $exception)
+    {
+        if($exception instanceof ModelNotFoundException){
+            return new JsonResponse([
+                'message' => "Unable to locate the {$this->prettyModelNotFound($exception)} you requested."
+            ], 404);
+        }
+
+        return parent::render($request, $exception);
+    }
+
+    /**
+     * @param Throwable $exception
+     * @return string
+     * @noinspection PhpPossiblePolymorphicInvocationInspection
+     */
+    private function prettyModelNotFound(Throwable $exception): string
+    {
+        try {
+            if( ! is_null($exception->getModel()))
+            {
+                return Str::lower(
+                    ltrim(
+                        preg_replace(
+                            '/[A-Z]/',
+                            ' $0',
+                            (new ReflectionClass($exception->getModel()))->getShortName()
+                        )
+                    )
+                );
+            }
+        } catch (ReflectionException $e) {
+            report($e);
+        }
+        return 'resource';
     }
 }
