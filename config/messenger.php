a2925f1308c3f349ea11e7a0fa06a324729306d8
Tippin <richard.tippin@gmail.com>
2021-10-02 03:49:31 -0400

    composer update, add new config in messenger.php

--- config/messenger.php
+++ config/messenger.php
@@ -237,6 +237,26 @@ return [
 
     /*
     |--------------------------------------------------------------------------
+    | Thread Verifications | Friendship Checks
+    |--------------------------------------------------------------------------
+    |
+    | Enable or disable friendship checks for threads.
+    |
+    | If enabled for private threads, the thread will be marked as pending upon
+    | creation if the two participants are not friends. The recipient will then
+    | have the option to accept or deny the new private thread request.
+    |
+    | If enabled for group threads, only friends of the active participant may
+    | be added to the group, otherwise any valid messenger provider may be
+    | added as a participant.
+    */
+    'thread_verifications' => [
+        'private_thread_friendship' => env('MESSENGER_VERIFY_PRIVATE_THREAD_FRIENDSHIP', true),
+        'group_thread_friendship' => env('MESSENGER_VERIFY_GROUP_THREAD_FRIENDSHIP', true),
+    ],
+
+    /*
+    |--------------------------------------------------------------------------
     | Message Edits
     |--------------------------------------------------------------------------
     |
