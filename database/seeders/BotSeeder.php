3c4833e55d7568472f7f7b7db98ccd3e2abb7985
Tippin <richard.tippin@gmail.com>
2022-01-14 22:26:07 -0500

    Refactor BotSeeder.php

--- database/seeders/BotSeeder.php
+++ database/seeders/BotSeeder.php
@@ -5,32 +5,18 @@ namespace Database\Seeders;
 use App\Bots\RecursionBot;
 use App\Models\User;
 use Illuminate\Database\Seeder;
+use RTippin\Messenger\Actions\Bots\InstallPackagedBot;
+use RTippin\Messenger\Facades\Messenger;
+use RTippin\Messenger\MessengerBots;
 use RTippin\Messenger\Models\Bot;
 use RTippin\Messenger\Models\BotAction;
 use RTippin\Messenger\Models\Thread;
-use RTippin\MessengerBots\Bots\ChuckNorrisBot;
-use RTippin\MessengerBots\Bots\CoinTossBot;
-use RTippin\MessengerBots\Bots\CommandsBot;
-use RTippin\MessengerBots\Bots\DadJokeBot;
-use RTippin\MessengerBots\Bots\DocumentFinderBot;
-use RTippin\MessengerBots\Bots\GiphyBot;
-use RTippin\MessengerBots\Bots\InsultBot;
-use RTippin\MessengerBots\Bots\InviteBot;
-use RTippin\MessengerBots\Bots\JokeBot;
-use RTippin\MessengerBots\Bots\KanyeBot;
-use RTippin\MessengerBots\Bots\KnockBot;
-use RTippin\MessengerBots\Bots\LocationBot;
-use RTippin\MessengerBots\Bots\QuotableBot;
-use RTippin\MessengerBots\Bots\RandomImageBot;
-use RTippin\MessengerBots\Bots\ReactionBot;
 use RTippin\MessengerBots\Bots\ReplyBot;
-use RTippin\MessengerBots\Bots\RockPaperScissorsBot;
-use RTippin\MessengerBots\Bots\RollBot;
-use RTippin\MessengerBots\Bots\WeatherBot;
-use RTippin\MessengerBots\Bots\WikiBot;
-use RTippin\MessengerBots\Bots\YoMommaBot;
-use RTippin\MessengerBots\Bots\YoutubeBot;
+use RTippin\MessengerBots\Packages\GamesPackage;
+use RTippin\MessengerBots\Packages\JokesterPackage;
+use RTippin\MessengerBots\Packages\NeoPackage;
 use RTippin\MessengerFaker\Bots\FakerBot;
+use Throwable;
 
 class BotSeeder extends Seeder
 {
@@ -38,12 +24,31 @@ class BotSeeder extends Seeder
      * Run the database seeds.
      *
      * @return void
+     *
+     * @throws Throwable
      */
-    public function run()
+    public function run(): void
     {
         $admin = User::where('email', '=', DatabaseSeeder::Admin['email'])->first();
         $group = Thread::group()->first();
-        $bot = Bot::factory()->for($group)->owner($admin)->create([
+
+        Messenger::setProvider($admin);
+
+        $this->installMessengerBot($group, $admin);
+
+        $this->installPackagedBots($group);
+    }
+
+    /**
+     * @param  Thread  $thread
+     * @param  User  $admin
+     * @return void
+     *
+     * @throws Throwable
+     */
+    private function installMessengerBot(Thread $thread, User $admin): void
+    {
+        $bot = Bot::factory()->for($thread)->owner($admin)->create([
             'name' => 'Messenger Bot',
         ]);
 
@@ -55,8 +60,30 @@ class BotSeeder extends Seeder
                 ->match($action[1])
                 ->triggers($action[2])
                 ->payload($action[3] ?? null)
+                ->cooldown($action[4] ?? 0)
                 ->create();
         }
+    }
+
+    /**
+     * @param  Thread  $thread
+     * @return void
+     *
+     * @throws Throwable
+     */
+    private function installPackagedBots(Thread $thread): void
+    {
+        app(InstallPackagedBot::class)
+            ->withoutEvents()
+            ->execute($thread, GamesPackage::getDTO());
+
+        app(InstallPackagedBot::class)
+            ->withoutEvents()
+            ->execute($thread, JokesterPackage::getDTO());
+
+        app(InstallPackagedBot::class)
+            ->withoutEvents()
+            ->execute($thread, NeoPackage::getDTO());
     }
 
     /**
@@ -64,15 +91,13 @@ class BotSeeder extends Seeder
      */
     private function actions(): array
     {
-        $awesome = ['reaction' => ':100:'];
-        $thumbUp = ['reaction' => ':thumbsup:'];
-        $rofl = ['reaction' => ':rofl:'];
         $about = [
             'quote_original' => true,
             'replies' => [
-                'Main package: https://github.com/RTippin/messenger',
-                'Bots package: https://github.com/RTippin/messenger-bots',
+                'Core package: https://github.com/RTippin/messenger',
+                'Ready-made Bots: https://github.com/RTippin/messenger-bots',
                 'Faker commands package: https://github.com/RTippin/messenger-faker',
+                'UI package: https://github.com/RTippin/messenger-ui',
                 'Demo app: https://github.com/RTippin/messenger-demo',
                 ':robot::robot::robot::robot:',
             ],
@@ -85,33 +110,10 @@ class BotSeeder extends Seeder
         ];
 
         return [
-            [ChuckNorrisBot::class, 'exact:caseless', '!chuck'],
-            [CommandsBot::class, 'exact:caseless', '!commands|!c'],
-            [DadJokeBot::class, 'contains:caseless', '!dadjoke|daddy|dad'],
-            [DocumentFinderBot::class, 'starts:with:caseless', '!doc|!document'],
-            [GiphyBot::class, 'starts:with:caseless', '!gif|!giphy'],
-            [InsultBot::class, 'contains:any:caseless', '!insult|fuck|asshole|bitch'],
-            [JokeBot::class, 'exact:caseless', '!joke'],
-            [KanyeBot::class, 'exact:caseless', '!kanye'],
-            [KnockBot::class, 'contains:caseless', '!knock|knock|ding|dong|alert'],
-            [RandomImageBot::class, 'exact:caseless', '!image'],
-            [RockPaperScissorsBot::class, 'starts:with:caseless', '!rps'],
-            [RollBot::class, 'starts:with:caseless', '!r|!roll'],
-            [WeatherBot::class, 'starts:with:caseless', '!w|!weather'],
-            [WikiBot::class, 'starts:with:caseless', '!wiki'],
-            [YoutubeBot::class, 'starts:with:caseless', '!youtube'],
-            [YoMommaBot::class, 'contains:caseless', '!yomomma|mom|mother|mommy'],
-            [LocationBot::class, 'exact:caseless', '!location|!findMe|!whereAmI'],
-            [ReactionBot::class, 'contains:caseless', '100|nice|cool|wow|awesome', $awesome],
-            [ReactionBot::class, 'contains:caseless', '100|nice|cool|wow|awesome', $thumbUp],
-            [ReactionBot::class, 'contains:caseless', 'lmao|lol|ha|lmfao|rofl', $rofl],
-            [ReplyBot::class, 'contains:caseless', 'help|about|git|package|error', $about],
-            [ReplyBot::class, 'contains:caseless', 'hi|hello|test|testing|hallo', $hello],
-            [RecursionBot::class, 'exact:caseless', '!recursion'],
-            [QuotableBot::class, 'exact:caseless', '!quote|!inspire|!quotable'],
-            [CoinTossBot::class, 'starts:with:caseless', '!toss|!headsOrTails|!coinToss'],
-            [FakerBot::class, 'starts:with:caseless', '!faker'],
-            [InviteBot::class, 'exact:caseless', '!invite|!inv'],
+            [ReplyBot::class, MessengerBots::MATCH_CONTAINS_CASELESS, 'help|git|package|error', $about, 300],
+            [ReplyBot::class, MessengerBots::MATCH_CONTAINS_CASELESS, 'hi|hello|test|testing|hallo', $hello, 15],
+            [RecursionBot::class, MessengerBots::MATCH_EXACT_CASELESS, '!recursion'],
+            [FakerBot::class, MessengerBots::MATCH_STARTS_WITH_CASELESS, '!faker'],
         ];
     }
 }
